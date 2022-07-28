0ad1a4127f95dd7f85d2c3031d1d51b41aea4c5a
tolziplohu <sylvie.dirkswager@gmail.com>
2022-07-28 10:39:14 -0500

    Start analysis of inflow rate data

--- Analysis/SampleAnalysis.R
+++ Analysis/SampleAnalysis.R
@@ -5,8 +5,17 @@ library(viridis)
 #Set your working directory to the Analysis folder for your project
 
 #Read in the data
-initial_data <- read.table("sample_treatment/munged_basic.dat", h=T)
-final_update <- subset(initial_data, update == "1000")
+initial_data <- read.csv("sgp-treatment/munged_tasks.csv", h=T)
+final_update <- subset(initial_data, update == "50000")
 
 #Plot the host and symbiont interaction values by vertical transmission rate
-ggplot(data=final_update, aes(x=partner, y=donate, color=partner)) + geom_boxplot(alpha=0.5, outlier.size=0) + ylab("Final Interaction Value") + xlab("Partner") + theme(panel.background = element_rect(fill='white', colour='black')) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + guides(fill=FALSE) +ylim(-1,1) + scale_color_manual(name="Horizontal\nTransmission\nMutation Rate", values=viridis(2)) + facet_wrap(~treatment)
+(ggplot(data=final_update, aes(x=partner, y=task_NOT, color=partner))
+	+ geom_boxplot(alpha=0.5, outlier.size=0)
+	+ ylab("Completions of NOT in the last 5000 timesteps")
+	+ xlab("Partner")
+	+ theme(panel.background = element_rect(fill='white', colour='black'))
+	+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) 
+	+ guides(fill=FALSE) 
+	+ ylim(-1,1) 
+	+ scale_color_manual(name="Horizontal\nTransmission\nMutation Rate", values=viridis(2))
+	+ facet_wrap(~treatment))
