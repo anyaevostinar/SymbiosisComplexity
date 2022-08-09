b309a9fea411c05ce775ca5dda83444577e87fc5
tolziplohu <sylvie.dirkswager@gmail.com>
2022-08-09 11:53:42 -0500

    Run different vertical transmission rates with resources required

---
+++ Analysis/vertical-transmission-2-2022-08-08/SampleAnalysis.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+library(viridis)
+library(reshape2)
+library(gridExtra)
+
+#Set your working directory to the Analysis folder for your project
+
+#Read in the data
+initial_data <- read.csv("munged_tasks.csv", h=T)
+final_update <- subset(initial_data, update == "50000")
+final_update[final_update$vert == "No symbiont", c("vert", "partner")] <- list("", "nosym")
+
+tasks <- c("NOT", "NAND", "AND", "ORN", "OR", "ANDN", "NOR", "XOR", "EQU")
+task_columns <- lapply(tasks, function(x) paste0("task_", x))
+task_map <- tasks
+names(task_map) <- task_columns
+
+task_data <- melt(final_update, id.vars=c("update", "vert", "partner"), measure.vars=task_columns)
+ggplot(data=task_data, aes(x=factor(as.character(vert), levels=unique(vert)), y=value, color=partner)) +
+	geom_boxplot(alpha=0.5, outlier.size=0.25) +
+    ylab("Task completions in the last 5000 timesteps") +
+	xlab("Vertical transmission rate") +
+	theme(panel.background = element_rect(fill='white', colour='black')) +
+	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='top') +
+	guides(fill="none") +
+	scale_color_manual(name="", values=viridis(3), guide=guide_legend(direction='horizontal'), breaks=c("nosym", "host", "symbiont"), labels=c("Host without symbiont", "Host with symbiont", "Symbiont")) +
+	# scale_y_continuous(trans='log10') +
+	facet_wrap(~variable, scales="free", labeller=labeller(variable=task_map))
+
+donated_data <- melt(subset(final_update, partner=="symbiont"), id.vars=c("vert"), measure.vars=c("earned", "donated"))
+a <- ggplot(data=donated_data, aes(x=factor(as.character(vert), levels=unique(vert)), y=value, color=variable)) +
+	geom_boxplot(alpha=0.5, outlier.size=0.5) +
+    ylab("Total symbiont resources earned and donated") +
+	xlab("Vertical transmission rate") +
+	theme(panel.background = element_rect(fill='white', colour='black')) +
+	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='top') +
+	guides(fill="none") +
+	scale_color_manual(name="", values=viridis(2), guide=guide_legend(direction='horizontal'), breaks=c("earned", "donated"), labels=c("Symbiont earned resources", "Symbiont donated resources"))
+b <- ggplot(data=subset(final_update, partner=="symbiont"), aes(x=factor(as.character(vert), levels=unique(vert)), y=donate_calls)) +
+	geom_boxplot(alpha=0.5, outlier.size=0.5) +
+    ylab("Total times symbionts ran the donate instruction") +
+	xlab("Vertical transmission rate") +
+	theme(panel.background = element_rect(fill='white', colour='black')) +
+	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='top') +
+	guides(fill="none")# +
+c <- ggplot(data=subset(final_update, partner=="symbiont"), aes(x=factor(as.character(vert), levels=unique(vert)), y=mutualism)) +
+	geom_boxplot(alpha=0.5, outlier.size=0.5) +
+    ylab("Average mutualism") +
+    ylim(-1, 1) +
+	xlab("Vertical transmission rate") +
+	theme(panel.background = element_rect(fill='white', colour='black')) +
+	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='top') +
+	guides(fill="none")# +
+	#scale_color_manual(name="", values=viridis(2), guide=guide_legend(direction='horizontal'), breaks=c("earned", "donated"), labels=c("Symbiont earned resources", "Symbiont donated resources"))
+grid.arrange(a, arrangeGrob(b, c, ncol=2), nrow=2)
---
+++ Analysis/vertical-transmission-2-2022-08-08/munge_data.py
@@ -0,0 +1,48 @@
+import os.path
+import gzip
+
+folder = '../../Data/vertical-transmission-2-2022-08-08/'
+
+verts = ["NONE", 0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+reps = range(10,20)
+header = "vert,rep,update,partner,donate_calls,donated,earned,mutualism,task_NOT,task_NAND,task_AND,task_ORN,task_OR,task_ANDN,task_NOR,task_XOR,task_EQU"
+
+outputFileName = "munged_tasks.csv"
+
+outFile = open(outputFileName, 'w')
+outFile.write(header)
+
+for v in verts:
+    for r in reps:
+            fname = f"{folder}Tasks_VT_{v}_SEED{r}.data"
+            curFile = open(fname, 'r')
+            donatedFile = open(f"{folder}SymDonated_VT_{v}_SEED{r}.data", 'r')
+            donatedFile.readline()
+            mutualismFile = open(f"{folder}SymImpact_VT_{v}_SEED{r}.data", 'r')
+            mutualism = mutualismFile.readline().strip()
+            #print(f"----VT_{v}_SEED{r}----")
+            for line in curFile:
+                if (line[0] != "u"):
+                    sym = True
+                    vname = v
+                    if v == "NONE":
+                        vname = "No symbiont"
+                        sym = False
+
+                    splitline = line.split(',')
+                    outstring1 = f"\n{vname},{r},{splitline[0]}"
+                    dLine = donatedFile.readline().split(',')
+                    # update,earned,calls,donated
+                    #print(dLine)
+                    outstring2 = f"{outstring1},symbiont,{dLine[2].strip()},{dLine[3].strip()},{dLine[1].strip()},{mutualism}"
+                    outstring1 += ",host,0,0,0,NA"
+                    for i in range(1, len(splitline), 2):
+                        outstring1 += "," + splitline[i].strip()
+                        outstring2 += "," + splitline[i+1].strip()
+                    outFile.write(outstring1)
+                    if sym:
+                        outFile.write(outstring2)
+            curFile.close()
+            donatedFile.close()
+            mutualismFile.close()
+outFile.close()
