53987377617448ab07ba0b85309b6f65d04376ee
Mitchell Anderson <andersonm2@carleton.edu>
2022-08-15 09:18:57 -0500

    Adding modifications to squaring data runners

--- Analysis/mutualism-squares-2022-08-11/SampleAnalysis.R
+++ Analysis/mutualism-squares-2022-08-11/SampleAnalysis.R
@@ -10,6 +10,13 @@ initial_data <- read.csv("munged_tasks.csv", h=T)
 final_update <- subset(initial_data, update == "10000")
 final_update[final_update$vert == "No symbiont", c("vert", "partner")] <- list("", "nosym")
 
+b <- ggplot(data=subset(final_update, partner=="symbiont"), aes(x=factor(as.character(vert), levels=unique(vert)), y=donate_calls)) +
+	geom_boxplot(alpha=0.5, outlier.size=0.5) +
+    ylab("Total times symbionts ran the donate instruction") +
+	xlab("Vertical transmission rate") +
+	theme(panel.background = element_rect(fill='white', colour='black')) +
+	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='top') +
+	guides(fill="none")# +
 c <- ggplot(data=subset(final_update, partner=="symbiont"), aes(x=factor(as.character(vert), levels=unique(vert)), y=mutualism)) +
 	geom_boxplot(alpha=0.5, outlier.size=0.5) +
     ylab("Average mutualism") +
@@ -19,4 +26,5 @@ c <- ggplot(data=subset(final_update, partner=="symbiont"), aes(x=factor(as.char
 	theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='top') +
 	guides(fill="none")# +
 	#scale_color_manual(name="", values=viridis(2), guide=guide_legend(direction='horizontal'), breaks=c("earned", "donated"), labels=c("Symbiont earned resources", "Symbiont donated resources"))
-grid.arrange(a, arrangeGrob(b, c, ncol=2), nrow=2)
+	
+grid.arrange(arrangeGrob(b, c, ncol=2), nrow=2)
--- Analysis/mutualism-squares-2022-08-11/munge_data.py
+++ Analysis/mutualism-squares-2022-08-11/munge_data.py
@@ -5,7 +5,7 @@ folder = '../../Data/host-symbiont-squares-2022-08-09/'
 
 verts = [0.0, 1.0]
 reps = range(10,20)
-header = "vert,rep,update,partner,donate_calls,donated,earned,mutualism,task_SQU"
+header = "vert,rep,update,partner,donate_calls,donated,earned,mutualism,attempts_horiz,success_horiz,attempts_vert,task_NOT,task_NAND,task_AND,task_ORN,task_OR,task_ANDN,task_NOR,task_XOR,task_EQU"
 
 outputFileName = "munged_tasks.csv"
 
@@ -16,14 +16,17 @@ for v in verts:
     for r in reps:
             fname = f"{folder}Tasks_VT{v}_SEED{r}.data"
             curFile = open(fname, 'r')
-            #donatedFile = open(f"{folder}SymDonated_VT_{v}_SEED{r}.data", 'r')
-            #donatedFile.readline()
+            #donatedFile = open(f"{folder}SymDonated_VT{v}_SEED{r}.data", 'r')
+            #donatedFile.readline() # skip header
             mutualismFile = open(f"{folder}SymImpact_VT{v}_SEED{r}.data", 'r')
-            mutualism = mutualismFile.readline().strip()
-            print("Mutualism", mutualism)
+            transFile = open(f"{folder}TransmissionRates_VT{v}_SEED{r}.data", 'r')
+            transFile.readline() # skip header
             #print(f"----VT_{v}_SEED{r}----")
             for line in curFile:
                 if (line[0] != "u"):
+                    # Split mutualism across updates so we have somewhere to put each line,
+                    # even though all that data is really from the last update.
+                    mutualism = (mutualismFile.readline() or "NA").strip()
                     sym = True
                     vname = v
                     if v == "NONE":
@@ -33,9 +36,12 @@ for v in verts:
                     splitline = line.split(',')
                     outstring1 = f"\n{vname},{r},{splitline[0]}"
                     # update,earned,calls,donated
-                    #print(dLine)
+                    #dLine = donatedFile.readline().split(',')
+                    # update,attempts_horiz,success_horiz,attempts_vert
+                    tLine = transFile.readline().split(',')
                     outstring2 = f"{outstring1},symbiont,{mutualism}"
-                    outstring1 += ",host,0,0,0,NA"
+                    outstring2 += f",{tLine[1].strip()},{tLine[2].strip()},{tLine[3].strip()}"
+                    outstring1 += ",host,0,0,0,NA,NA,NA,NA"
                     for i in range(1, len(splitline), 2):
                         outstring1 += "," + splitline[i].strip()
                         outstring2 += "," + splitline[i+1].strip()
@@ -43,5 +49,7 @@ for v in verts:
                     if sym:
                         outFile.write(outstring2)
             curFile.close()
+            #donatedFile.close()
             mutualismFile.close()
 outFile.close()
+
