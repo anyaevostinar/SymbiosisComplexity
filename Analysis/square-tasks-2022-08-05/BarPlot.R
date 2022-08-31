a8487ce47c5e8bd41930093ebc4263eedf74a437
Mitchell Anderson <andersonm2@carleton.edu>
2022-08-31 08:45:57 -0500

    Changes to BarPlot

--- Analysis/square-tasks-2022-08-05/BarPlot.R
+++ Analysis/square-tasks-2022-08-05/BarPlot.R
@@ -8,7 +8,7 @@ final_df <- df
 
 
 
-p<-ggplot(subset(subset(final_df, update=="200000"), vertical_transmission=="0.8"), aes(x=as.factor(squareNum), y=completions,fill = partner)) + geom_bar(stat = "identity",position=position_dodge())  + scale_y_log10() + theme(axis.text.x = element_text(size =4, angle = 90, vjust = 0.5, hjust=1), panel.background = element_rect(fill = "white")) + labs(x = "Square Numbers", y = "Completions")
+p<-ggplot(subset(subset(final_df, update=="200000"), vertical_transmission=="0.8"), aes(x=as.factor(squareNum), y=completions,fill = partner)) + geom_bar(stat = "identity",position=position_dodge())  + scale_y_log10() + theme(axis.text.x = element_text(size =4, angle = 90, vjust = 0.5, hjust=1), panel.background = element_rect(fill = "white")) + labs(x = "Square Numbers", y = "Completions") + scale_fill_manual(values=c("purple", "yellow"), labels=c("Host", "Symbiont")) + theme(legend.position = "top")
 
 
 
